d4a89d6f7b47ec12dfbc34e55a1ee1594330c34b
lvenkatasai <87753517+lvenkatasai@users.noreply.github.com>
2023-07-18 15:26:37 +0530

    Add files via upload

---
+++ sketch.py
@@ -0,0 +1,11 @@
+# grey ,invert, blur -> sketch
+
+import cv2
+image = cv2.imread("rng.png")
+grey_img = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+invert = cv2.bitwise_not(grey_img)
+blur = cv2.GaussianBlur(invert,(21,21),0)
+invertedblur =cv2.bitwise_not(blur)
+sketch=cv2.divide(grey_img,invertedblur,scale = 256.0)
+
+cv2.imwrite("dr1.png",sketch)
